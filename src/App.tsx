6371ed5de1f49d24106c7fe9967e0b47c14fd31e
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2024-03-23 21:26:31 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+   return (
+      <div>
+         <h1 className='text-5xl text-red-600 bg-black'>A Rspack + React template with TailwindCss & Prettier.</h1>
+      </div>
+   )
+}
